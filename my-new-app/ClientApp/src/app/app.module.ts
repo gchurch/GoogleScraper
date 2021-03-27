fa8dd64c765609ade913e37c86dab9a567a84042
George Church <georgechurch10@gmail.com>
2021-03-27 14:49:22 +0000

    Deleted unnecessary angular components.

--- my-new-app/ClientApp/src/app/app.module.ts
+++ my-new-app/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { SearchComponent } from './search/search.component';
 
 @NgModule({
@@ -16,8 +14,6 @@ import { SearchComponent } from './search/search.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     SearchComponent
   ],
   imports: [
@@ -26,8 +22,6 @@ import { SearchComponent } from './search/search.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'search', component: SearchComponent}
     ])
   ],
