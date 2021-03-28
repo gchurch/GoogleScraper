05a0dae6ef31827aa762c587e541aaa8117b0225
George Church <georgechurch10@gmail.com>
2021-03-28 12:15:27 +0100

    Added a unit test to test the SearchService class.

---
+++ UnitTestProject1/SearchServiceTests.cs
@@ -0,0 +1,46 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using my_new_app.Services;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SearchServiceTests
+    {
+        private List<string> searchResultUrls = new List<string>()
+        {
+            "www.facebook.com",
+            "www.google.com",
+            "www.twitter.com",
+            "www.amazon.com",
+            "www.infotrack.co.uk",
+            "www.infotrack.co.uk",
+            "www.wikipedia.co.uk"
+        };
+        private string keywords = "test test test";
+
+        private SearchService CreateSearchServiceForTesting()
+        {
+            Mock<IGoogleSearchService> googleSearchServiceMock = new Mock<IGoogleSearchService>();
+            googleSearchServiceMock.Setup(x => x.GetUrlsFromGoogleSearch(keywords))
+                .Returns(searchResultUrls);
+            SearchService searchService = new SearchService(googleSearchServiceMock.Object);
+            return searchService;
+        }
+
+        [TestMethod]
+        public void GetUrlPositions_GivenInfoTrackUrl()
+        {
+            // Arrange
+            SearchService searchService = CreateSearchServiceForTesting();
+
+            // Act
+            string url = "www.infotrack.co.uk";
+            string positions = searchService.GetUrlPositions(keywords, url);
+
+            // Assert
+            Assert.AreEqual("5, 6", positions);
+        }
+    }
+}
