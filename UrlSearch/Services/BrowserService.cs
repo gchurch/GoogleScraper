0c1ee8c2fc38e4a37afa0f96c9c1c26d30471a85
George Church <georgechurch10@gmail.com>
2021-04-01 17:02:25 +0100

    Created a wrapper around Selenium.

---
+++ UrlSearch/Services/BrowserService.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlSearch.Services
+{
+    public class BrowserService : IBrowserService
+    {
+        private ChromeDriver _browserDriver;
+
+        public BrowserService()
+        {
+            CreateBrowserDriver();
+        }
+
+        private void CreateBrowserDriver()
+        {
+            var options = new ChromeOptions() { };
+            options.AddArguments(new List<string>() { "headless", "disable-gpu" });
+            _browserDriver = new ChromeDriver(options);
+        }
+
+        ~BrowserService()
+        {
+            QuitBrowserDriver();
+        }
+
+        private void QuitBrowserDriver()
+        {
+            _browserDriver.Quit();
+        }
+
+        public void NavigateToUrl(string url)
+        {
+            _browserDriver.Navigate().GoToUrl(url);
+        }
+
+        public List<string> ScrapeTextInCiteTags()
+        {
+            var citeTags = _browserDriver.FindElementsByTagName("cite");
+            List<string> citeTexts = new List<string>();
+            foreach (var citeTag in citeTags)
+            {
+                citeTexts.Add(citeTag.Text);
+            }
+            return citeTexts;
+        }
+    }
+}
