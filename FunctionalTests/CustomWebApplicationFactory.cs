8ef6c14a02c416e614d909fda85d853f98e5880b
George Church <georgechurch10@gmail.com>
2021-04-02 16:29:59 +0100

    Added some functional tests.

---
+++ FunctionalTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using UrlSearch.Services;
+
+namespace FunctionalTests
+{
+    public class CustomWebApplicationFactory<TStartup> 
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(IBrowserService));
+
+                services.Remove(descriptor);
+
+                services.AddTransient<IBrowserService, FakeBrowserService>();
+
+                var sp = services.BuildServiceProvider();
+            });
+        }
+    }
+}
---
+++ FunctionalTests/FakeBrowserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrlSearch.Services;
+
+namespace FunctionalTests
+{
+    public class FakeBrowserService : IBrowserService
+    {
+        public void NavigateToUrl(string url)
+        {   
+        }
+
+        public List<string> ScrapeTextInCiteTags()
+        {
+            return new List<string>()
+            {
+                "www.facebook.com",
+                "www.infotrack.co.uk",
+                "www.google.com",
+                "www.twitter.com",
+                "www.amazon.com",
+                "www.infotrack.co.uk",
+                "www.infotrack.co.uk",
+                "www.wikipedia.co.uk"
+            };
+        }
+    }
+}
