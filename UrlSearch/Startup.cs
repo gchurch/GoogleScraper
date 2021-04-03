804efc649f4dfe3ce316b877862e764dd0a31cf5
George Church <georgechurch10@gmail.com>
2021-04-03 10:28:37 +0100

    Stopped using the proxy development server.

--- UrlSearch/Startup.cs
+++ UrlSearch/Startup.cs
@@ -71,8 +71,8 @@ namespace UrlSearch
 
                 if (env.IsDevelopment())
                 {
-                    //spa.UseAngularCliServer(npmScript: "start");
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                    spa.UseAngularCliServer(npmScript: "start");
+                    //spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
